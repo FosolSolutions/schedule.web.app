b1ec6da8c595374d2cd7a409197ca627fece2391
Matthew Bennett <mattsbennett@gmail.com>
2018-09-15 21:56:08 -0700

    [#] Initial prototype of landing page and user dashboard views. Add and implement react-router. Build changes. Also, remove jest coverage report from repo and ignore it.

--- Scheduler/Client/src/features/app/components/App/App.js
+++ Scheduler/Client/src/features/app/components/App/App.js
@@ -4,6 +4,8 @@
 import PropTypes from "prop-types";
 import React from "react";
 import { connect } from "react-redux";
+import classNames from "classnames";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 //------------------------------------------------------------------------------
 // Redux Support
@@ -15,16 +17,26 @@ import { selectCalendars } from "redux/reducers/calendarsReducer";
 // Components
 //------------------------------------------------------------------------------
 import Button from "@material-ui/core/Button";
+import Divider from "@material-ui/core/Divider";
 import Drawer from "@material-ui/core/Drawer";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
 import IconButton from "@material-ui/core/IconButton";
 import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
 
 //------------------------------------------------------------------------------
 // Assets
 //------------------------------------------------------------------------------
 import AccountCircle from "@material-ui/icons/AccountCircle";
+import ArrowBack from "@material-ui/icons/ArrowBack";
 import Event from "@material-ui/icons/Event";
-import MoreVert from "@material-ui/icons/MoreVert";
+import Home from "@material-ui/icons/Home";
+import Menu from "@material-ui/icons/Menu";
+import coEventLogo from "assets/images/logos/coEventLogoWh.svg";
+import hands from "assets/images/hands.svg";
 import styles from "features/app/components/App/app.scss";
 
 //------------------------------------------------------------------------------
@@ -38,49 +50,177 @@ export class App extends React.Component {
         super(props);
 
         this.state = {
-            drawerIsOpen: false
+            drawerIsOpen: false,
+            loginRedirect: false
         };
 
         props.setCalendars();
     }
 
     /**
-     * Handle menu button clicks
+     * Handle menu button click
      */
     handleMenuButtonClick() {
         this.setState({ drawerIsOpen: !this.state.drawerIsOpen });
     }
 
+    /**
+     * Handle login button click
+     */
+    handleLoginToggleClick() {
+        this.setState({ loginRedirect: !this.state.loginRedirect });
+    }
+
     render() {
-        return [
+        const appBarClassNames = classNames({
+            [styles.appBar]: true,
+            [styles.drawerIsOpen]: this.state.drawerIsOpen
+        });
+        const paperClassNames = classNames({
+            [styles.paper]: true,
+            [styles.paperIsOpen]: this.state.drawerIsOpen
+        });
+        const menuButton = this.state.drawerIsOpen ? (
+            false
+        ) : (
+            <IconButton
+                className={styles.iconButton}
+                onClick={() => this.handleMenuButtonClick()}
+            >
+                <Menu />
+            </IconButton>
+        );
+        const home = () => [
             <AppBar
-                className={styles.appBar}
-                position={"static"}
+                className={appBarClassNames}
+                position={"fixed"}
                 key="mainAppBar"
             >
-                <div className={styles.leftContainer}>
-                    <Event />
-                    <h1 className={styles.appTitle}>EventStack</h1>
-                </div>
-                <IconButton
-                    className={styles.iconButton}
-                    onClick={() => this.handleMenuButtonClick()}
-                >
-                    <MoreVert />
-                </IconButton>
+                <Toolbar className={styles.toolbar}>
+                    <div className={styles.leftContainer}>
+                        <img className={styles.logo} src={coEventLogo} alt="" />
+                    </div>
+                    <div className={styles.rightContainer}>
+                        <Button
+                            className={styles.login}
+                            variant={"text"}
+                            onClick={() => this.handleLoginToggleClick()}
+                        >
+                            Login
+                        </Button>
+                        <Button className={styles.signUp} variant={"contained"}>
+                            Sign Up
+                        </Button>
+                    </div>
+                </Toolbar>
+            </AppBar>,
+            <div className={styles.splashScreen} key="splashScreen">
+                <h1 className={styles.valueProp}>
+                    Flexible scheduling for teams and organizations.
+                </h1>
+                <img className={styles.hands} src={hands} alt="" />
+            </div>,
+            <div className={styles.spacer} key="spacer2" />
+        ];
+        const user = () => [
+            <AppBar
+                className={appBarClassNames}
+                position={"absolute"}
+                key="mainAppBar"
+            >
+                <Toolbar className={styles.toolbar} disableGutters={true}>
+                    <div className={styles.leftContainer}>{menuButton}</div>
+                    <div className={styles.rightContainer}>
+                        <Button
+                            className={styles.login}
+                            onClick={() => this.handleLoginToggleClick()}
+                        >
+                            Logout
+                        </Button>
+                    </div>
+                </Toolbar>
             </AppBar>,
             <Drawer
-                anchor="right"
+                className={styles.drawer}
+                classes={{
+                    paper: paperClassNames
+                }}
                 key="navDrawer"
                 open={this.state.drawerIsOpen}
                 onClose={() => this.handleMenuButtonClick()}
+                variant="permanent"
             >
-                <Button>
-                    <span className={styles.buttonLabel}>Account</span>
-                    <AccountCircle />
-                </Button>
+                <div className={styles.backHeader}>
+                    <div className={styles.backWrap}>
+                        {
+                            <IconButton
+                                onClick={() => this.handleMenuButtonClick()}
+                            >
+                                <ArrowBack />
+                            </IconButton>
+                        }
+                    </div>
+                </div>
+                <Divider />
+                <List className={styles.list}>
+                    <ListItem button className={styles.listItem}>
+                        <ListItemIcon>
+                            <Home />
+                        </ListItemIcon>
+                        <ListItemText className={styles.listItemText}>
+                            Home
+                        </ListItemText>
+                    </ListItem>
+                    <Divider />
+                    <ListItem button className={styles.listItem}>
+                        <ListItemIcon>
+                            <Event />
+                        </ListItemIcon>
+                        <ListItemText className={styles.listItemText}>
+                            Schedules
+                        </ListItemText>
+                    </ListItem>
+                    <Divider />
+                    <ListItem button className={styles.listItem}>
+                        <ListItemIcon>
+                            <AccountCircle />
+                        </ListItemIcon>
+                        <ListItemText className={styles.listItemText}>
+                            Account
+                        </ListItemText>
+                    </ListItem>
+                </List>
             </Drawer>
         ];
+
+        return (
+            <Router>
+                <div className={styles.fullHeight}>
+                    <Route
+                        exact
+                        path="/"
+                        render={() =>
+                            this.state.loginRedirect ? (
+                                <Redirect push to="/user" />
+                            ) : (
+                                home()
+                            )
+                        }
+                    />
+                    <Route
+                        exact
+                        path="/user"
+                        render={() =>
+                            this.state.loginRedirect ? (
+                                user()
+                            ) : (
+                                <Redirect push to="/" />
+                            )
+                        }
+                    />
+                </div>
+            </Router>
+        );
     }
 }
 
